bf92f5fbceb1e3025ab84f5dccb1c694be6c0616
Michael Verrilli <msv@pobox.com>
2015-09-06 14:12:41 -0400

    Added in the basic amqp parser

--- tcp_middle_man.py
+++ tcp_middle_man.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import select
+from basic_amqp_parser import basic_amqp_parser
 
 listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -13,6 +14,12 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.connect(('127.0.0.1',5672))
 
+server_parser = basic_amqp_parser('SERVER')
+next(server_parser)
+
+client_parser = basic_amqp_parser('CLIENT')
+next(client_parser)
+
 while True:
 
     ready_sockets = select.select([server, client], [], [])[0]
@@ -21,13 +28,14 @@ while True:
         data = server.recv(16384)
         if len(data) > 0:
             client.send(data)
+            server_parser.send(data)
         else:
             break
-        print 'SERVER BYTES', ":".join("{:02x}".format(ord(c)) for c in data)
+
     if client in ready_sockets:
         data = client.recv(16384)
-        if len(data):
+        if len(data) > 0:
             server.send(data)
+            client_parser.send(data)
         else:
             break
-        print 'CLIENT BYTES', ":".join("{:02x}".format(ord(c)) for c in data)
